30eaea62d188e6ceef077e80820f66d52d906fb6
sijun <sionwer5@gmail.com>
2025-06-17 22:36:56 +0900

    feat: map height 조정

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -76,7 +76,7 @@ export default function Map() {
     return (
         <div
             ref={mapElement}
-            style={{ width: "100%", height: "400px" }}
+            style={{ width: "100%", height: "500px" }}
         />
     )
 }
